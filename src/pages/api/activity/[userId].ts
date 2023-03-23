d9ba94d8f557ef797c8f2b032603ab881bb114c5
Yohann THEPAUT <contact@ythepaut.com>
2023-03-23 22:37:28 +0100

    feat(IOT-21): add list activity route

--- src/pages/api/activity/[userId].ts
+++ src/pages/api/activity/[userId].ts
@@ -1,8 +1,13 @@
 import {
+    BadRequestException,
     Body,
     createHandler,
     Get,
+    HttpCode,
+    ParseDatePipe,
+    ParseNumberPipe,
     Post,
+    Query,
     ValidationPipe,
 } from "next-api-decorators";
 import {
@@ -13,11 +18,40 @@ import clientPromise from "../../../services/server/mongodb";
 
 class ActivityHandler {
     @Get()
-    getActivity() {
-        return [];
+    @HttpCode(200)
+    getActivity(
+        @Query("dateForm", ParseDatePipe({ nullable: true }))
+        queryDateFrom?: Date,
+        @Query("dateTo", ParseDatePipe({ nullable: true })) queryDateTo?: Date,
+        @Query("limit", ParseNumberPipe({ nullable: true }))
+        queryLimit?: number,
+        @Query("page", ParseNumberPipe({ nullable: true })) queryPage?: number,
+    ) {
+        const dateFrom = queryDateFrom ?? new Date(0);
+        const dateTo = queryDateTo ?? new Date(8640000000000000); // latest possible date ((2^63-1) / 2)
+        const limit = queryLimit ?? 100;
+        const page = queryPage ?? 1;
+
+        if (limit < 1) throw new BadRequestException("Limit must be >= 1");
+        if (page < 1) throw new BadRequestException("Page must be >= 1");
+
+        return new Promise(async (resolve) => {
+            let db = (await clientPromise).db("INFO902");
+            let results = await db
+                .collection(ACTIVITY_COLLECTION_NAME)
+                .find({
+                    dateFrom: { $gte: dateFrom, $lte: dateTo },
+                })
+                .skip((page - 1) * limit)
+                .limit(limit)
+                .map((activity) => ({ ...activity, _id: undefined }))
+                .toArray();
+            resolve(results);
+        });
     }
 
     @Post()
+    @HttpCode(201)
     createActivity(@Body(ValidationPipe) body: CreateActivityDTO) {
         return new Promise(async (resolve) => {
             let db = (await clientPromise).db("INFO902");

1f929dff839e3db7a9107b496f246e1fd7f7d188
Yohann THEPAUT <contact@ythepaut.com>
2023-03-29 15:13:33 +0200

    refactor: move logic outside of controller

--- src/pages/api/activity/index.ts
+++ src/pages/api/activity/index.ts
@@ -10,14 +10,11 @@ import {
     Query,
     ValidationPipe,
 } from "next-api-decorators";
-import { getActivityLevel } from "../../../services/server/userActivity";
+import { CreateActivityDTO } from "../../../types/dto/activity";
 import {
-    Activity,
-    ACTIVITY_COLLECTION_NAME,
-    CreateActivityDTO,
-} from "../../../types/dto/activity";
-import clientPromise from "../../../services/server/mongodb";
-import getConfig from "next/config";
+    createNewActivity,
+    getActivityList,
+} from "../../../services/server/activity";
 
 class ActivityHandler {
     @Get()
@@ -30,8 +27,6 @@ class ActivityHandler {
         queryLimit?: number,
         @Query("page", ParseNumberPipe({ nullable: true })) queryPage?: number,
     ) {
-        const { serverRuntimeConfig } = getConfig();
-
         const dateFrom = queryDateFrom ?? new Date(0);
         const dateTo = queryDateTo ?? new Date(8640000000000000); // latest possible date ((2^63-1) / 2)
         const limit = queryLimit ?? 100;
@@ -40,51 +35,15 @@ class ActivityHandler {
         if (limit < 1) throw new BadRequestException("Limit must be >= 1");
         if (page < 1) throw new BadRequestException("Page must be >= 1");
 
-        return new Promise(async (resolve) => {
-            let db = (await clientPromise).db(
-                serverRuntimeConfig.mongodbDatabase,
-            );
-            let results = await db
-                .collection(ACTIVITY_COLLECTION_NAME)
-                .find({
-                    //dateFrom: { $gte: dateFrom, $lte: dateTo },
-                })
-                .sort({ dateStart: -1 })
-                .skip((page - 1) * limit)
-                .limit(limit)
-                .map((activity) => ({ ...activity, _id: undefined }))
-                .toArray();
-            resolve(results);
-        });
+        return getActivityList(page, limit, dateFrom, dateTo);
     }
 
     @Post()
     @HttpCode(201)
     createActivity(@Body(ValidationPipe) body: CreateActivityDTO) {
-        const { serverRuntimeConfig } = getConfig();
-
         return new Promise(async (resolve) => {
-            let db = (await clientPromise).db(
-                serverRuntimeConfig.mongodbDatabase,
-            );
-            const activity: Activity = {
-                dateStart: new Date(Date.now()),
-                dateEnd: new Date(Date.now() - body.duration!),
-                accelerations: body.accelerations!.map((a) => ({
-                    acceleration: {
-                        x: a.acceleration!.x,
-                        y: a.acceleration!.y,
-                        z: a.acceleration!.z,
-                    },
-                    dt: a.dt,
-                })),
-                orientation: body.orientation!,
-            };
-
-            await db.collection(ACTIVITY_COLLECTION_NAME).insertOne(activity);
-            resolve({
-                level: await getActivityLevel(db),
-            });
+            await createNewActivity(body);
+            resolve({});
         });
     }
 }
---
+++ src/services/server/activity.ts
@@ -0,0 +1,55 @@
+import {
+    Activity,
+    ACTIVITY_COLLECTION_NAME,
+    CreateActivityDTO,
+} from "../../types/dto/activity";
+import clientPromise from "./mongodb";
+import getConfig from "next/config";
+
+export const getActivityList = (
+    page: number,
+    limit: number,
+    dateFrom: Date,
+    dateTo: Date,
+): Promise<Activity[]> => {
+    const { serverRuntimeConfig } = getConfig();
+    return new Promise(async (resolve) => {
+        let db = (await clientPromise).db(serverRuntimeConfig.mongodbDatabase);
+        let results = await db
+            .collection(ACTIVITY_COLLECTION_NAME)
+            .find({
+                //dateFrom: { $gte: dateFrom, $lte: dateTo },
+            })
+            .sort({ dateStart: -1 })
+            .skip((page - 1) * limit)
+            .limit(limit)
+            .map((activity) => ({ ...activity, _id: undefined }))
+            .toArray();
+        resolve(results as unknown as Activity[]);
+    });
+};
+
+export const createNewActivity = (
+    activityDto: CreateActivityDTO,
+): Promise<void> => {
+    const { serverRuntimeConfig } = getConfig();
+
+    return new Promise(async (resolve) => {
+        let db = (await clientPromise).db(serverRuntimeConfig.mongodbDatabase);
+        const activity: Activity = {
+            dateStart: new Date(Date.now()),
+            dateEnd: new Date(Date.now() - activityDto.duration!),
+            accelerations: activityDto.accelerations!.map((a) => ({
+                acceleration: {
+                    x: a.acceleration!.x,
+                    y: a.acceleration!.y,
+                    z: a.acceleration!.z,
+                },
+                dt: a.dt,
+            })),
+            orientation: activityDto.orientation!,
+        };
+        await db.collection(ACTIVITY_COLLECTION_NAME).insertOne(activity);
+        resolve();
+    });
+};
